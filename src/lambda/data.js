a3ddb377cf062d067183b177a325a125dbc6be3b
Luis Augusto <luis.augusto@hdmz.com>
2018-12-05 11:26:40 -0800

    Finished setting up first lambda function

--- src/lambda/data.js
+++ src/lambda/data.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 exports.handler = function(event, context, callback) {
-  fs.readdir('./', function(err, filenames) {
+  fs.readdir('/', function(err, filenames) {
     callback(null, {
       statusCode: 200,
       body: JSON.stringify(filenames)

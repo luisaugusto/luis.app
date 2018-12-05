5ebc8d31846e580a055639b5131c9bb7a05bc304
Luis Augusto <luis.augusto@hdmz.com>
2018-12-05 11:55:52 -0800

    Finished setting up first lambda function

--- src/lambda/data.js
+++ src/lambda/data.js
@@ -4,7 +4,7 @@ exports.handler = function(event, context, callback) {
   fs.readdir('./' + event.queryStringParameters.data, function(err, filenames) {
     callback(null, {
       statusCode: 200,
-      body: JSON.stringify(process.env) + JSON.stringify(filenames)
+      body: JSON.stringify(process.env) + JSON.stringify(filenames) + process.cwd()
     });
   });
 };

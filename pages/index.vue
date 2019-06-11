690e7c2a3c307b744f681ffc353255405663bc9c
Luis Augusto <hello@luis.codes>
2019-06-10 19:28:10 -0700

    workingo n api

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,8 @@
 <script>
 import AppSection from '../components/AppSection.vue';
 
+console.log('fullenv -');
+console.log(process.env);
 console.log('hi');
 console.log('id ' + process.env.CONTENTFUL_ACCESS_TOKEN);
 

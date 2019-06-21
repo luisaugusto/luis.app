a465973ebf4413acb062ce2c4d5000a8bb1f8d6e
Luis Augusto <hello@luis.codes>
2019-06-21 16:55:32 -0700

    removed fetch

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <app-section type="Description" :header="false" />
     <app-section type="Blog" custom-header="Latest Posts"/>
-    <!-- <app-section type="Skillsets" /> -->
+    <app-section type="Skillsets" />
   </div>
 </template>
 
@@ -12,14 +12,7 @@ import { createClient } from '~/plugins/contentful.js';
 const client = createClient();
 
 export default {
-  async fetch ({ store, params }) {
-    let { items } = await client.getEntries({
-      content_type: 'blogPost',
-      order: 'fields.postDate'
-    });
 
-    store.commit('setPosts', items);
-  },
 	components: {
 		AppSection
 	}

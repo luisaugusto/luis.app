ac829c354cfb5da069ef0442feb3b24ef4662c83
Luis Augusto <hello@luis.codes>
2019-06-21 16:42:20 -0700

    bug fix

--- pages/index.vue
+++ pages/index.vue
@@ -8,8 +8,18 @@
 
 <script>
 import AppSection from '../components/AppSection.vue';
+import { createClient } from '~/plugins/contentful.js';
+const client = createClient();
 
 export default {
+  async fetch ({ store, params }) {
+    let { items } = await client.getEntries({
+      content_type: 'blogPost',
+      order: 'fields.postDate'
+    });
+
+    store.commit('setPosts', items);
+  },
 	components: {
 		AppSection
 	}
